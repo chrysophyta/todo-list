a092d60cc174a8f4f89a21ce8121204d77b97092
Vivian <chrysophyta@gmail.com>
2018-06-04 23:01:50 -0400

    add navigation

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import './App.css';
+import Nav from './Nav';
+
+class App extends Component {
+  render() {
+    return <Nav />;
+  }
+}
+
+export default App;
