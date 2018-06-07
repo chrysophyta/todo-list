495ae27a1eaf30e0ecad56b46cad25087a7178bb
Vivian <chrysophyta@gmail.com>
2018-06-07 01:38:34 -0400

    add feather icons

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -7,13 +7,23 @@ class ListItem extends Component {
         <div className="list-labels">
           <input type="checkbox" />
           <h3 className="item-title">Title</h3>
-          <div>☆</div>
-          <div>✎</div>
+          <span>
+            <i data-feather="star" />
+          </span>
+          <span>
+            <i data-feather="edit-2" />
+          </span>
         </div>
         <div className="content-icons">
-          <div>⚐</div>
-          <div>☻</div>
-          <div>✌︎</div>
+          <span>
+            <i data-feather="message-square" />
+          </span>
+          <span>
+            <i data-feather="file" />
+          </span>
+          <span>
+            <i data-feather="clock" />
+          </span>
         </div>
       </li>
     );
