47aeaa7cb1399f181770b843bf5c793d3eae8112
Vivian <chrysophyta@gmail.com>
2018-08-14 19:29:26 -0400

    feat: render icon with correspond data

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -2,37 +2,41 @@ import React, { Component } from 'react';
 import Icons from './icons';
 
 class ListItem extends Component {
-  handleClick = (key) => {
-    this.props.toggleItem(this.props.data.id,key);
+  handleClick = key => {
+    this.props.toggleItem(this.props.data.id, key);
   };
-
+  renderIcons = () => {
+    if (this.props.data.content) {
+      return <Icons iconName="message-square" />;
+    }
+    if (this.props.data.due) {
+      return (
+        <span>
+          <Icons iconName="clock" />
+          {this.props.data.due}
+        </span>
+      );
+    }
+  };
   render() {
     const { data } = this.props;
     return (
       <li className="list-item">
         <div className="list-labels">
-          <input type="checkbox" 
-                 onChange={()=>this.handleClick('completed')} 
-                 checked={data.completed} />
+          <input
+            type="checkbox"
+            onChange={() => this.handleClick('completed')}
+            checked={data.completed}
+          />
           <h3 className="item-title">{data.title}</h3>
-          <span onClick={()=>this.handleClick('starred')}>
+          <span onClick={() => this.handleClick('starred')}>
             <Icons iconName="star" starred={data.starred} />
           </span>
           <span>
             <Icons iconName="edit-2" />
           </span>
         </div>
-        <div className="content-icons">
-          <span>
-            <Icons iconName="message-square" />
-          </span>
-          <span>
-            <Icons iconName="file" />
-          </span>
-          <span>
-            <Icons iconName="clock" />
-          </span>
-        </div>
+        <div className="content-icons">{this.renderIcons()}</div>
       </li>
     );
   }
